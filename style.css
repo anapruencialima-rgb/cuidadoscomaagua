16fd5f4b069c55341e680b82529f8d1ebca539b9
anapruencialima-rgb <ana.pruencia.lima@escola.pr.gov.br>
2026-06-08 10:27:40 -0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,77 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+
+body{
+    font-family: Arial, sans-serif;
+    background: #eaf8ff;
+    color:#333;
+}
+
+header{
+    background: #0099cc;
+    color:white;
+    text-align:center;
+    padding:30px;
+}
+
+main{
+    width:90%;
+    max-width:1000px;
+    margin:30px auto;
+}
+
+.card{
+    background:white;
+    padding:25px;
+    margin-bottom:20px;
+    border-radius:12px;
+    box-shadow:0 4px 10px rgba(0,0,0,0.1);
+}
+
+h2{
+    color:#0077aa;
+    margin-bottom:15px;
+}
+
+ul{
+    margin-left:20px;
+    margin-bottom:15px;
+}
+
+button{
+    background:#0099cc;
+    color:white;
+    border:none;
+    padding:10px 20px;
+    border-radius:8px;
+    cursor:pointer;
+    transition:0.3s;
+}
+
+button:hover{
+    background:#0077aa;
+}
+
+input{
+    width:100%;
+    padding:10px;
+    margin:10px 0;
+    border:1px solid #ccc;
+    border-radius:8px;
+}
+
+#resultado,
+#dica-extra{
+    margin-top:15px;
+    font-weight:bold;
+}
+
+footer{
+    text-align:center;
+    background:#0099cc;
+    color:white;
+    padding:15px;
+}
